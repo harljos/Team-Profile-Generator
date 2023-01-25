3dd1e7e796de62970b3e3700e8f0471086d7eb63
unknown <joseph.harley02@gmail.com>
2023-01-25 17:25:00 -0500

    removed dist from gitignore and made skeleton for employee tests and engineer tests

---
+++ _tests_/Employee.test.js
@@ -0,0 +1,37 @@
+const Profile = require("../index");
+
+describe("Employee", () => {
+    describe("Initialization", () => {
+        it("should create an object with name, id, and email", () => {
+
+        });
+
+        it("should throw an error if no arguments are provided", () => {
+
+        });
+    });
+
+    describe("getName", () => {
+        it("should return name of Employee object", () =>  {
+
+        });
+    });
+
+    describe("getId", () => {
+        it("should return Id of Employee object", () => {
+
+        });
+    });
+
+    describe("getEmail", () => {
+        it("should return email of Employee object", () => {
+
+        });
+    });
+
+    describe("getRole", () => {
+        it("should return role of Employee object", () => {
+
+        });
+    });
+});
---
+++ _tests_/Engineer.test.js
@@ -0,0 +1,24 @@
+const Profile = require("../index");
+
+describe("Engineer", () => {
+    describe("Initialization", () => {
+        it("should create an object with github", () => {
+
+        });
+        it("should throw an error if no argument are given", () => {
+
+        });
+    });
+
+    describe("getGithub", () => {
+        it("should return github from Engineer object", () => {
+
+        });
+    });
+
+    describe("getRole", () => {
+        it("should return role from Engineer object", () => {
+
+        });
+    });
+});
