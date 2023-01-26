28eeb1471a4e2e52264050aebc1bc6f81c17ffc8
unknown <joseph.harley02@gmail.com>
2023-01-25 20:54:52 -0500

    added tests for Engineer class

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -1,24 +1,32 @@
-const Profile = require("../index");
+const Engineer = require("../src/engineer");
 
 describe("Engineer", () => {
     describe("Initialization", () => {
-        it("should create an object with github", () => {
+        it("should create an object with name, id, email, and github", () => {
+            const employee = new Engineer({name: "Joe", id: "1", email: "j@gmail.com", github: "harljos"});
 
+            expect(employee.getName()).toEqual({name: "Joe", id: "1", email: "j@gmail.com", github: "harljos"});
         });
         it("should throw an error if no argument are given", () => {
+            const cb = () => new Engineer();
 
+            expect(cb).toThrow();
         });
     });
 
     describe("getGithub", () => {
         it("should return github from Engineer object", () => {
+            const employee = new Engineer({name: "Joe", id: "1", email: "j@gmail.com", github: "harljos"});
 
+            expect(employee.getGithub()).toEqual("harljos");
         });
     });
 
     describe("getRole", () => {
         it("should return role from Engineer object", () => {
+            const employee = new Engineer({name: "Joe", id: "1", email: "j@gmail.com", github: "harljos"});
 
+            expect(employee.getRole).toEqual("Engineer");
         });
     });
 });
