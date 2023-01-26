0915edb7fbae9913fb9251ba5b3c8ad1ae20039c
unknown <joseph.harley02@gmail.com>
2023-01-26 17:45:48 -0500

    created a function that will keep asking the user the employee question until the user selects the I am done building my team

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // required neccessary packages
 const inquirer = require("inquirer");
-const Employee = require("./lib/employee");
 const Engineer = require("./lib/engineer");
 const Intern = require("./lib/intern");
 const Manager = require("./lib/manager");
 
+const employees = [];
+
+// questions that will ask user for input for manager attributes
 const managerQuestions = [
     {
         type: "input",
@@ -26,4 +28,40 @@ const managerQuestions = [
         message: "What is the manager's office number",
         name: "officeNumber"
     }
-]
+];
+
+const employeeQuestion = [
+    {
+        type: "list",
+        message: "What employee would you like to hire?",
+        name: "employee",
+        choices: ["engineer", "intern", "I am done building my team"]
+    }
+];
+
+// prempts question for which employee the user would like to add
+const employeePromt = () => {
+    inquirer.prompt(employeeQuestion)
+            .then((data) => {
+                if (data.employee === "engineer" || data.employee === "intern") {
+                    employeePromt();
+                }
+                else {
+                    console.log(data);
+                }
+            })
+}
+
+// runs when file is initialized
+const init = () => {
+    inquirer
+        .prompt(managerQuestions)
+        .then((data) => {
+            console.log(data);
+        })
+        .then(() => {
+            employeePromt();
+        })    
+}
+
+init();
