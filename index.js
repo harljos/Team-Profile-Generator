c52616b437f1bd8d43b9a7f11f396f8df8d7156a
unknown <joseph.harley02@gmail.com>
2023-01-26 16:37:25 -0500

    added questions for manager object

---
+++ index.js
@@ -0,0 +1,29 @@
+// required neccessary packages
+const inquirer = require("inquirer");
+const Employee = require("./lib/employee");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+const Manager = require("./lib/manager");
+
+const managerQuestions = [
+    {
+        type: "input",
+        message: "What is the manager's name?",
+        name: "managerName"
+    },
+    {
+        type: "input",
+        message: "What is the manager's id?",
+        name: "managerId"
+    },
+    {
+        type: "input",
+        message: "What is the manager's email?",
+        name: "managerEmail"
+    },
+    {
+        type: "input",
+        message: "What is the manager's office number",
+        name: "officeNumber"
+    }
+]
